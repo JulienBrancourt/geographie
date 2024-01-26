613b4baf289ee4bf76e8e4a8a4a6a01d394bf369
ElyanisJ <brancourt.julien@hotmail.fr>
2024-01-26 15:10:48 +0100

    mise en forme, useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-function App() {
-  return (
-    <div>
+import { useState, useEffect } from "react";
+//https://restcountries.com/v3.1/region/europe
 
-    </div>
-  )
+
+function App() {
+  const [ListCountries, setListCountries] = useState(null)
+
+	return (
+		<div className="min-h-screen bg-slate-800">
+			<div className="max-w-7xl mx-auto py-20 px-4">
+        <h1 className="text-gray-50 text-4xl">Europe Countries Data</h1>
+        <p className="text-gray-100 text-xl mb-8"> Click !</p>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
