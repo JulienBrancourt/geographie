e62f3222801219668971202e9555f0aff023566e
ElyanisJ <brancourt.julien@hotmail.fr>
2024-01-30 18:17:50 +0100

    appel API et tri avec .sort et localeCompare

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { data } from "autoprefixer";
 import { useState, useEffect } from "react";
 //https://restcountries.com/v3.1/region/europe
 
 
 function App() {
-  const [ListCountries, setListCountries] = useState(null)
+const [ListCountries, setListCountries] = useState(null);
+
+useEffect(() => {
+	fetch("https://restcountries.com/v3.1/region/europe")
+		.then((data) => data.json())
+		.then((data) => {
+			console.log(data);
+			data.sort((a, b) => a.name.common.localeCompare(b.name.common))
+				// if (a.name.common < b.name.common) {
+				// 	return -1;
+				// } else if (a.name.common > b.name.common) {
+				// 	return 1;
+				// }
+			// });
+			setListCountries(data);
+		});
+}, []);
+
 
 	return (
 		<div className="min-h-screen bg-slate-800">
